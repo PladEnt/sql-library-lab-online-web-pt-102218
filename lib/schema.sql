742685496fe78ac0edc04a9daf68292490b5b0a5
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-19 15:16:43 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,3 +14,19 @@ CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year TEXT,
+  series_id TEXT,
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT
+);
+
+
